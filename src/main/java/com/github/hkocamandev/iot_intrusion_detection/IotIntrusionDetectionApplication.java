04f79b4401eb3c5f13fe1278d1abb65a53e3b623
HKocaman <hasankocamandev@gmail.com>
2026-06-17 14:53:14 +0300

    feat: initial project structure

---
+++ src/main/java/com/github/hkocamandev/iot_intrusion_detection/IotIntrusionDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.github.hkocamandev.iot_intrusion_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotIntrusionDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotIntrusionDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hkocamandev/iot_intrusion_detection/IotIntrusionDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hkocamandev.iot_intrusion_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotIntrusionDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
